c081a7e858325a9292c1fa9795cdae518d447e11
Vaibhav Shekhar <shekharvaibhav6@gmail.com>
2024-03-09 23:04:54 +0530

    tictac

---
+++ tictac/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Grid from './components/Grid/Grid';
+
+function App() {
+  
+
+  return (
+    <>
+    
+    <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
